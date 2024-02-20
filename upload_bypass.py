ba7254fba018295dd64b0b8f1ff702c1a21b28de
Sagiv Michael <81802295+sAjibuu@users.noreply.github.com>
2024-02-20 15:22:36 +0200

    Update upload_bypass.py

--- upload_bypass.py
+++ upload_bypass.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
 # Made by Sagiv
-# Brute forcing technique to bypass file upload restrictions
-# The tool requires all its assets, do not try to use it without it
+
+# Upload Bypass is a simple tool designed to assist Pentesters and Bug Hunters in testing file upload mechanisms.
+
+# Copyright (C) 2024 Sagiv Michael
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # importing necessary modules
 import argparse
